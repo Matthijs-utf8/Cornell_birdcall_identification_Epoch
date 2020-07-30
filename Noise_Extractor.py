84bdc8c42b987b3ac7c4bc67b7827cdd3e3fbe79
Matthijs-utf8 <65110282+Matthijs-utf8@users.noreply.github.com>
2020-07-30 09:26:03 +0200

    added cleaned up version of the file

--- Noise_Extractor.py
+++ Noise_Extractor.py
@@ -5,46 +5,17 @@ Created on Wed Jul  1 21:09:24 2020
 @author: Matthijs Schrage
 """
 
-"""
-Samenvatting van de file:
-	
-	Een file die een audio bestand laadt en de pure noise kan scheiden van de non-pure noise.
-	Als we eenmaal van elk bestand pure-noise hebben 
-		kunnen we heel simpel een spectral noise gate gebruiken om de noise te filteren.
-		
-	De heuristic waar ik nu mee aan het spelen ben is energy, omdat dat de meest gebruikte en belangrijkste is.
-	We moeten echter een veel beter heuristic gebruiken om dit echt goed te laten werken.
-	
-	Works in progress:
-		- Energy coefficient automatisch berekenen
-		- Heuristic verbeteren
-		- Een validation methode bedenken waarmee we verschillende heuristics kunnen vergelijken op prestatie
-"""
-
 import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import librosa
-from librosa.feature import zero_crossing_rate, spectral_centroid, spectral_rolloff, spectral_bandwidth, rms
-from librosa.onset import onset_detect, onset_backtrack, onset_strength
 import sklearn
 import warnings
-import scipy
-from scipy import fftpack
-from scipy.signal.windows import hamming
 import sounddevice as sd
-import math
-import time
 
 # A function that prevents warnings when loading in files with librosa
 warnings.simplefilter("ignore")
-
-with open(".config") as config_file:
-	config_lines = config_file.readlines()
-	split = (line.split("=", 1) for line in config_lines)
-	base_dir = next(value for key, value in split if key == "data_folder") + "/"
-
 
 # A helper function for normalizing signals. It helps with viualisation to get everything on the same scale.
 def normalize(x, axis=0):
@@ -55,8 +26,7 @@ def autocorr(x, t=1):
 	return np.corrcoef(np.array([x[:-t], x[t:]]))
 
 # Add the path of each file to the train.csv
-
-# base_dir = os.path.join(os.path.expanduser("~"), "Downloads/birdsong-recognition/")
+base_dir = os.path.join(os.path.expanduser("~"), "Downloads/birdsong-recognition/")
 df_train = pd.read_csv(base_dir + "train.csv")
 
 ####### !!!!!!!!!!!!!!! ##########
@@ -64,165 +34,133 @@ df_train = pd.read_csv(base_dir + "train.csv")
 # df_train['full_path'] = base_dir + "train_audio/" + df_train['ebird_code'] + '/' + df_train['filename']
 # pd.to_csv(base_dir + "train.csv")
 
-###### Hyper parameters ######
-
-plotting = False
-
-### Data ###
-y, sr = librosa.load(df_train['full_path'][3])
-
-#Gives a measure of how much noise is in the file
-autocorrelation = autocorr(y)[0,1]
-print("Autocorrelation: " + str(autocorrelation))
-# sd.play(y, sr)
-
-### Window function ###
-window_width = 2048
-stepsize = 512
-# Calculate the frames. Skips the data at the end of the last frame does not fit perfectly.
-nr_of_frames = (len(y) - window_width + stepsize) // stepsize
-# if window_width != stepsize:
-# 	raise ValueError("De functie werkt alleen (nog) als deze twee hetzelfde zijn")
-# Hamming window has proven to be the best window for birdsounds from the papers I (Matthijs) have read
-window = hamming(window_width)
-
-##############################
-
-def get_frames(samples):
+""" Split a soundwave up in frames """
+def get_frames(samples, window_width, stepsize):
+	
+	nr_of_frames = (len(samples) - window_width + stepsize) // stepsize
 	
 	frames = np.array([samples[stepsize*n:stepsize*n+window_width] for n in range(nr_of_frames)])
 	
-	return frames
+	return nr_of_frames, frames
 
+""" Multiply a series of frames with a window function (hammig window)"""
 def window_function_transform(frames):
 	
-	new_frames = []
+	from scipy.signal.windows import hamming
 	
-	# For every frame transofrm the samples according to the window function
-	for frame in frames:
-		
-		# Caculate the newly windowed frame
-		new_frame = frame * window
-		
-		new_frames.append(new_frame)
+	# Construct a Hamming window with the same number of datapoints as 1 frame. 
+	window = hamming(frames.shape[1])
+	
+	# Multiply all frames with our window function
+	new_frames = np.array([frame*window for frame in frames])
 	
 	return new_frames
 
-def get_statistcal_features(frames):
+""" Compute the statistical features of each frame that we need for our noise/non-noise heuristic """
+def get_statistcal_features(frames, heuristic="energy"):
 	
 	# Initiate the necessary lists where we want to store our features
-	energies = []
-	zero_crossing_rates = []
+	energies = np.array([np.sum(frame**2) for frame in frames])
 	
-	# For every frame, compute the features of the frame
-	for frame in frames:
-		
-		# Calculate the energy for the newly windowed frame
-		window_energy = np.sum(frame**2)
-	
-		# Calculate the zero crossing rate for the newly windowed frame
-		zcr = np.sum(librosa.core.zero_crossings(frame))/window_width
-		
-		# Append the features that we want
-		energies.append(window_energy)
-		zero_crossing_rates.append(zcr)
-	
-	return energies, zero_crossing_rates
+	return energies, np.mean(energies)
 
-frames = window_function_transform( get_frames( y ) )
-
-energies, zero_crossing_rates = get_statistcal_features( frames )
-
-################
-# We will have to think of a better heuristic than just the energy of a signal
-# Possible other candidates: zero_crossing_rate, spectral
-################
-
-def divide_noise_non_noise(energies):
+""" Helps us with the automatic calculation of the energy coeff. for each file. The higher the S/N is, the higher the energy coeff. is. """
+def compute_energy_coefficient(samples, base_coefficient=1):
 	
-	# Get the mean energy of the signal
-	mean_energy = np.mean(energies)
+	# Compute an approximation of the Signal-to-Noise-Ratio
+	SNR = np.abs( np.log10( np.abs( ( np.mean(samples) ) / ( np.std(samples) ) ) ) )
 	
-	# Initiate lists for storing non_birdcall and birdcall sounds
+	# Compute the energy coefficient
+	base_coefficient = base_coefficient
+	energy_coefficient = base_coefficient * ( ( 1 / (SNR) ) ** 2 )
+	
+	return SNR, energy_coefficient
+
+""" The function that bring it all together. """
+def get_noise_frames(fullpath, window_width=2048, stepsize=512, plotting=False):
+	
+	""" Preparation for separating pure noise from non-pure noise. """
+	
+	# Read in the audiofile with librosa.
+	samples, sampling_rate = librosa.load(fullpath)
+	
+	# Separate the samples in frames according to the window_width and stepsize
+	nr_of_frames, frames = get_frames(samples, window_width=window_width, stepsize=stepsize)
+	
+	# Use a window function (hamming works best) on all our frames
+	frames = window_function_transform(frames)
+	
+	# Get the statistical features that we need. For now only 'energy' works.
+	energies, mean_energy = get_statistcal_features( frames )
+	
+	# Get the energy coefficient that we need for separating pure noise from non-pure noise.
+	SNR, energy_coefficient = compute_energy_coefficient(samples, base_coefficient=2)
+	
+	print("Energy coefficient: " + str(energy_coefficient))
+	print("Signal-to-Noise: " + str(SNR))
+	
+	""" Separating pure noise from non-pure noise. """
+	
+	# Initiate lists to store the separated frames in.
 	noisy_frames = []
 	non_noisy_frames = []
+	noisy_energy = []
+	non_noisy_energy = []
 	
+	# Go through all of the frame-energies. The ones below a certain threshold have a very high chance of being pure background noise.
 	for index, energy in enumerate(energies):
 		
-		coeff = 0.05
-		
-		###############
-		# AUTOMATISEREN van de coefficient. 
-		# Ik zit te denken aan de coefficient afleiden van autocorrelatie. 
-		# Die twee lijken elkaar te beïnvloeden.
-		# De optimale coefficienten tussen files kunnen verschillen van 0.02 tot 0.3 ofzo
-		# Ik vermoed dat het te maken heeft met de Signal-to-Noise-Ratio (SNR)
-		# Ik bepaal hem nu elke keer met manual, door te luisteren naar de "noisy_frames"
-		################
-		if energy < coeff * mean_energy:
+		if energy < energy_coefficient * mean_energy:
 			
-			#Add the pure noisy frames to the appropriate list
-			noisy_frames.extend(frames[index][:stepsize])
-		
+			# Add the pure noisy parts to the appropriate list
+			noisy_frames.extend(frames[index][int((window_width-stepsize)/2):int((window_width+stepsize)/2)])
+			noisy_energy.append(energy)
 		else:
-			#Add the non-noise frames to the appropriate list
-			non_noisy_frames.extend(frames[index])
+			# Add the non-noise frames to the appropriate list
+			non_noisy_frames.extend(frames[index][int((window_width-stepsize)/2):int((window_width+stepsize)/2)])
+			non_noisy_energy.append(energy)
 	
+	# A measure for how well the noise is predictable (higher is better). The better predictable it is, the better a spectral noise gate will work
+	print("Noise predictability: " + str(autocorr(noisy_frames)[0,1]/autocorr(non_noisy_frames)[0,1]))
+	
+	""" Plotting """
+	
+	if plotting == True:
+		
+		# Initiate time domain axes for some different graphs
+		t_soundwave = np.linspace(0, len(samples)/sampling_rate, len(samples))
+		t_soundwave_noisy = np.linspace(0, len(noisy_frames)/sampling_rate, len(noisy_frames))
+		t_soundwave_non_noisy = np.linspace(0, len(non_noisy_frames)/sampling_rate, len(non_noisy_frames))
+		
+		t_windowed_features = np.linspace(0, len(samples)/sampling_rate, nr_of_frames)
+		t_windowed_features_noisy = np.linspace(0, len(noisy_frames)/sampling_rate, len(noisy_energy))
+		t_windowed_features_non_noisy = np.linspace(0, len(non_noisy_frames)/sampling_rate, len(non_noisy_energy))
+		
+		#Plot the signal versus the signal energy
+		plt.figure(figsize=(20,12))
+		plt.title("Energy whole signal")
+		plt.plot(t_soundwave, normalize(samples), alpha=0.5)
+		plt.plot(t_windowed_features, normalize(energies))
+		plt.show()
+		
+		#Plot the signal versus the signal energy
+		plt.figure(figsize=(20,12))
+		plt.title("Energy pure noise signal")
+		plt.plot(t_soundwave_noisy, normalize(noisy_frames), alpha=0.5)
+		plt.plot(t_windowed_features_noisy, normalize(noisy_energy))
+		plt.show()
+		
+		#Plot the signal versus the signal energy
+		plt.figure(figsize=(20,12))
+		plt.title("Energy non pure noise signal")
+		plt.plot(t_soundwave_non_noisy, normalize(non_noisy_frames), alpha=0.5)
+		plt.plot(t_windowed_features_non_noisy, normalize(non_noisy_energy))
+		plt.show()
+		
+	#Uncomment this when you want to play the noisy or non noisy sounds at the end
+	sd.play(non_noisy_frames, sampling_rate)
+		
 	return noisy_frames, non_noisy_frames
 
-noisy_frames, non_noisy_frames = divide_noise_non_noise(energies)
-
-#############
-# Dit stuk met de fourier transforms gebruik ik om de noisy en non noisy frames
-# met elkaar te vergelijken. Weet niet of dit zo gaat lukken,
-# maar ik probeer door verbanden te vinden een optimale manier te bereiken
-# om pure noise van non pure noise te scheiden.
-#############
-print(autocorr(noisy_frames)[0,1])
-print(autocorr(non_noisy_frames)[0,1])
-
-fft_samples_noise = np.fft.fft(noisy_frames, n=200000)
-fft_samples_birds = np.fft.fft(non_noisy_frames, n=200000)
-
-print(np.corrcoef(fft_samples_noise, fft_samples_birds).real[0,1])
-
-frequencies_noise = np.abs(np.fft.fftfreq(len(fft_samples_noise), d=1/sr))
-frequencies_birds = np.abs(np.fft.fftfreq(len(fft_samples_birds), d=1/sr))
-
-if plotting:
-	### Plotting stuff ###
-	t_soundwave = np.linspace(0, len(y)/sr, len(y))
-	t_windowed_features = np.linspace(0, len(y)/sr, nr_of_frames)
-	
-	#Plot the signal versus the signal energy
-	plt.figure(figsize=(20,12))
-	plt.title("Energy")
-	plt.plot(t_soundwave, y, alpha=0.5)
-	plt.plot(t_windowed_features, normalize(energies))
-	plt.show()
-	
-	#Plot the signal versus the zero crossing rate
-	plt.figure(figsize=(20,12))
-	plt.title("Zero crossing rate")
-	plt.plot(t_soundwave, y, alpha=0.5)
-	plt.plot(t_windowed_features, normalize(zero_crossing_rates))
-	plt.show()
-	
-	plt.figure(figsize=(20,12))
-	plt.title("Noise fft")
-	plt.plot(frequencies_noise, fft_samples_noise)
-	plt.show()
-	
-	plt.figure(figsize=(20,12))
-	plt.title("Birdsounds fft")
-	plt.plot(frequencies_birds, fft_samples_birds)
-	plt.show()
-
-# A test that shows we can generate a spectrogram and convert it back to the orignal sound
-if False:
-	spectrogram = np.fft.fft(non_noisy_frames)
-	converted_back = np.fft.ifft(spectrogram).astype('float')
-
-	sd.play(converted_back, sr)
-
-	sd.wait()
+if __name__ == "__main__":
+	get_noise_frames(df_train['full_path'][4080], plotting=True)
