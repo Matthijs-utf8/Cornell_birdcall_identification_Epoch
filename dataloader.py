a5ce5c501aaac6592ecf488c85cacd3cdc5c2057
thomas <thomas.wiggers@student.uva.nl>
2020-08-06 20:33:10 +0200

    unfix path fix that didnt fix it. Fixed dataloader

--- dataloader.py
+++ dataloader.py
@@ -2,15 +2,16 @@ import glob
 import numpy as np
 from tensorflow import keras
 
+from baseline_nn import bird_code
+
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
 
-    def __init__(self, data_root, batch_size=32, dim=(512, 512), n_channels=1, shuffle=True):
+    def __init__(self, data_root, batch_size=32, dim=(16,7,2048), shuffle=True):
         'Initialization'
         self.batch_size = batch_size
         self.dim = dim
-        self.n_channels = n_channels
         self.shuffle = shuffle
 
         self.data_root = data_root
@@ -44,15 +45,23 @@ class DataGenerator(keras.utils.Sequence):
     def __data_generation(self, files_temp):
         'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size,), dtype=str)
+        batch_size = min(self.batch_size, len(files_temp))
+
+        X = np.empty((batch_size, *self.dim))
+        y = np.empty((batch_size,), dtype=int)
 
         # Generate data
         for i, file in enumerate(files_temp):
             # Store sample
-            X[i,] = np.load(self.data_root + "/" + file)
+
+            X[i,] = np.load(file)
 
             # Store class
-            y[i] = file.split("/")[-1].split("_")[0]
+            bird_name = file.split("/")[-1].split("_")[0]
+            y[i] = bird_code[bird_name]
+        print("y", y)
+        return X, keras.utils.to_categorical(y, num_classes=len(bird_code))
 
-        return X, y
+if __name__ == '__main__':
+    d = DataGenerator("preprocessed")
+    print(d[0])
