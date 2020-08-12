427e8ed23ff180d8affcb1e4d4f1c875239332f5
thomas <thomas.wiggers@student.uva.nl>
2020-08-12 13:30:17 +0200

    added testset generator and testing pipeline

--- dataloader.py
+++ dataloader.py
@@ -57,10 +57,13 @@ class DataGenerator(keras.utils.Sequence):
         for i, file in enumerate(files_temp):
             # Store sample
 
+
             try:
-                X[i,] = np.load(file)
+                data = np.load(file)
             except ValueError as e:
                 raise ValueError("Malformed numpy file:" + file) from e
+
+            X[i,] = data
 
             # Store class
             bird_name = file.split("/")[-1].split("_")[0]
