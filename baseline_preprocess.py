2719608b7e3e9978b88f1c5fa0e5a4879b077c7a
aeduin <aeduin@nanquanu.org>
2020-08-12 20:24:19 +0200

    improved baseline_preprocess readability and comments:

--- baseline_preprocess.py
+++ baseline_preprocess.py
@@ -18,66 +18,74 @@ universal_sample_rate = 22000
 spectrogram_slices_per_input = universal_sample_rate * 5 // window_size # = 5 seconds
 
 def preprocess(file_path, feature_extractor: keras.models.Model):
+    """
+    Loads the audio file, generates a spectrogram, and applies feature_extractor on it
+    """
     spectrograms = tf_fourier(file_path)
 
     if spectrograms != []:
+        # Duplicate the single amplitude channel to 3 channels, because ResNet50 expects 3 channels
         spectrograms = np.array(spectrograms)
-        print(spectrograms.shape)
-
         spectrograms = np.reshape(spectrograms, spectrograms.shape + (1,))
         spectrograms = np.repeat(spectrograms, 3, axis=3)
-
+        
+        # Apply the feature extractor
         return feature_extractor.predict(spectrograms)
     else:
         return []
 
 def tf_fourier(file_path):
+    """
+    Applies the short-time fourier transform implemented on the GPU by TensorFlow
+    """
     sound, sample_rate = librosa.load(file_path)
 
+    # Make sure all files have the same sample rate
     if sample_rate != universal_sample_rate:
         sound = resample(sound, int(universal_sample_rate * (len(sound) / sample_rate)))
         pass
-
+    
+    # Generate the spectrogram
     spectrogram = tf.abs(
-        tf.signal.stft(tf.reshape(sound, [1, -1]), window_size, window_size, fft_length=512)
+        tf.signal.stft(tf.reshape(sound, [1, -1]), window_size, window_size)
     )[0]
 
-    slices = []
-    for i in range(spectrogram.shape[0] // spectrogram_slices_per_input):
-        spectrogram_slice = spectrogram[i * spectrogram_slices_per_input : (i + 1) * spectrogram_slices_per_input]
-        slices.append(spectrogram_slice)
-        
-        # print(spectrogram_slice.shape)
-        # plt.imshow(spectrogram_slice)
-        # plt.show()
+    # Split up into slices of (by default) 5 seconds
+    slices = [
+        spectrogram[
+             i      * spectrogram_slices_per_input :
+            (i + 1) * spectrogram_slices_per_input
+        ]
+        for i in range(spectrogram.shape[0] // spectrogram_slices_per_input)
+    ]
 
     return slices
 
-    
-
-
-spectrogram_dim = (250, 257)
-resnet: keras.models.Model = ResNet50(input_shape=(spectrogram_dim[0], spectrogram_dim[1], 3), include_top=False)
+spectrogram_shape = (250, 257)
+resnet: keras.models.Model = ResNet50(input_shape=(spectrogram_shape + (3,)), include_top=False)
 
 if __name__ == "__main__":
     import sys
     import os
 
-    output_folder = "preprocessed2"
+    output_dir = "preprocessed2"
     
-    if not os.path.isdir(output_folder):
-        os.mkdir(output_folder)
+    # Create output_dir if it doesn't exist yet
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
+    # Process all files based on the birdcodes in the arguments
     for birdcode in sys.argv[1:]:
         print(birdcode)
-        fragment_id = 0
 
+        fragment_id = 0   # A unique identifier for each slice of 5 seconds
         path = data_reading.test_data_base_dir + "train_audio/" + birdcode + "/"
+
         for file_name in tqdm(os.listdir(path)):
             fragments = preprocess(path + file_name, resnet)
 
             for fragment in fragments:
-                np.save(output_folder + "/" + birdcode + "_" + str(fragment_id), fragment)
+                np.save(output_dir + "/" + birdcode + "_" + str(fragment_id), fragment)
                 fragment_id += 1
 
 
