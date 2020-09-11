21c8d15f0b1009b81dfdaba636bd8901b4b7d4e4
thomas <thomas.wiggers@student.uva.nl>
2020-09-11 18:11:28 +0200

    hes hdf5 for training

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("--arch", default="resnet-full", type=str,
                         help="Network architecture, possible values: 'cnn', 'resnet-head', or '1d-conv' or 'resnet-full")
     parser.add_argument("--name", type=str, help="The experiment run name for tensorboard")
+    parser.add_argument("--dataset", type=str, help="The dataset for this experiment")
 
     args = parser.parse_args()
 
@@ -51,19 +52,19 @@ if __name__ == "__main__":
 
 
     # Data
-    data_generator = dataloader.DataGenerator("spectrograms", batch_size=args.batch_size, dim=input_shape,
-                                              channels=channels)
-    data_generator, data_generator_val = data_generator.split(0.1)
+    with dataloader.DataGeneratorHDF5(args.dataset) as data_generator:
 
-    reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2,
-                                                  patience=5, cooldown=2, min_lr=1e-9)
+        data_generator, data_generator_val = data_generator.split(0.1)
 
-    tensorboard_callback = utils.LRTensorBoard(log_dir=f"logs/{args.name}", settings_to_log=str(args))
+        reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2,
+                                                      patience=5, cooldown=2, min_lr=1e-9)
 
-    save_best_callback = keras.callbacks.ModelCheckpoint(filepath="models/" + args.name + ".val_f1.{val_f1_m:.3f}.h5",
-                                                         save_best_only=True, monitor='val_f1_m')
-    # callback = keras.callbacks.EarlyStopping(monitor='val_f1_m', patience=5)
+        tensorboard_callback = utils.LRTensorBoard(log_dir=f"logs/{args.name}", settings_to_log=str(args))
 
-    model.fit(data_generator, callbacks=[reduce_lr, tensorboard_callback, save_best_callback],
-              epochs=args.epochs, workers=args.workers, validation_data=data_generator_val)
-    model.save("models/" + args.name + ".h5")
+        save_best_callback = keras.callbacks.ModelCheckpoint(filepath="models/" + args.name + ".val_f1.{val_f1_m:.3f}.h5",
+                                                             save_best_only=True, monitor='val_f1_m')
+        # callback = keras.callbacks.EarlyStopping(monitor='val_f1_m', patience=5)
+
+        model.fit(data_generator, callbacks=[reduce_lr, tensorboard_callback, save_best_callback],
+                  epochs=args.epochs, workers=args.workers, validation_data=data_generator_val)
+        model.save("models/" + args.name + ".h5")
