bcd88875e22194828855d732eb9ee9d0682d4308
Dilara <dilara.tank1@student.uva.nl>
2020-09-14 11:41:56 +0200

    bufgix

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 
             print('X shape \n', X.shape)
             # print('X array\n', X)
-            y = y[;,;,3]
+            y = np.repeat(y, 3, axis=2)
             print('y shape \n', y.shape)
             # print('y array \n', y)
             model.fit(X, y, batch_size=args.batch_size, epochs=args.epochs)
