3ed3f27fc7d428ff133578b87cde09c4384a8ac2
aeduin <aeduin@nanquanu.org>
2020-09-14 20:24:13 +0200

    using cuda for pytorch training

--- train_pytorch.py
+++ train_pytorch.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--lr", default=0.0001, type=float, help="Learning rate")
     parser.add_argument("--epochs", default=50, type=int, help="Number of epochs to train for")
-    parser.add_argument("--batch-size", default=512, type=int, help="Training batch size")
+    parser.add_argument("--batch-size", default=4, type=int, help="Training batch size")
     parser.add_argument("--workers", default=1, type=int, help="Number of dataloader workers, may work incorrectly")
     parser.add_argument("--model-weights", default=None, type=str, help="The path to the file from which to load weights")
     parser.add_argument("--model-name", type=str, help="Name of the model architecture that will be trained")
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     model = None
 
     if args.model_name == "resnet50-pretrained":
-        model = pytorch_models.ResNet()# .to('cuda:0')
+        model = pytorch_models.ResNet().to('cuda:0')
 
         weights_path = args.model_weights
 
@@ -40,22 +40,34 @@ if __name__ == "__main__":
 
     total_loss = 0.0
 
-    with dataloader.DataGeneratorHDF5Pytorch(args.data_path) as dataloader:
+    with dataloader.DataGeneratorHDF5Pytorch(args.data_path, batch_size=args.batch_size) as dataloader:
         for epoch in range(args.epochs):
             print("epoch =", epoch)
             for data in dataloader:
+                # print("start new data")
             # for data in tqdm(dataloader):
                 inputs, labels = data
-                # labels = labels.to("cuda:0")
-                # inputs = inputs.to("cuda:0")
+                # print("inputs size =", inputs.size())
+                # print("labels size =", labels.size())
+
+                # print("split data")
+                labels = labels.to("cuda:0")
+                inputs = inputs.to("cuda:0")
 
                 optimizer.zero_grad()
+                # print("zero'ed optimizer gradients")
 
                 outputs = model(inputs)
+                # print("calculated outputs")
                 
                 loss = criterion(outputs, labels.float())
+                # print("calculated loss")
+
                 loss.backward()
+                # print("back propagated")
+
                 optimizer.step()
+                # print("optimizer executed")
 
                 total_loss += loss.item()
                 time_since_print_loss += 1
